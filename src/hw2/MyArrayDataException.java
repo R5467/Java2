d740a3b46561dfd5648b1a4d2a7a1e23abeed467
InnaOs <R_musavarov@mail.ru>
2022-03-29 16:35:47 +0300

    hw2

---
+++ src/hw2/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package hw2;
+
+class MyArrayDataException extends RuntimeException {
+    MyArrayDataException(String msg) {
+        super("Ошибка преобразования элемента массива в целое число.\n" + " " + msg);
+    }
+
+}
---
+++ src/hw2/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package hw2;
+
+class MyArraySizeException extends RuntimeException {
+    MyArraySizeException(String msg) {
+        super("Ошибка размерности массива.\n" + " " + msg);
+    }
+
+}
